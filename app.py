78675c3c5ce199657555ba497731cb6750ace6a1
Steve Dusty <kuantingk2@gmail.com>
2020-12-18 09:59:40 -0800

    Fixed app.py

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return send_from_directory("site", "index.html")
+    return send_from_directory("site", "templates/index.html")
 
-    
-    if __name__ == "__main__":
-        app.run(debug=True)
+@app.route("/<path:path>")
+def other(path):
+    return send_from_directory("site", path)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
