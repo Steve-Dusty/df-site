11899300f811cd3d50e8c86c36c17b9c1c8a5c4f
Steve-Dusty <kuantingk2@gmail.com>
2020-12-17 22:51:44 -0800

    up-to-date across all devices

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+alert("right"); 
+function toggler() {
+    var x = document.getElementsByClassName("column1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+function toggler2() {
+    var y = document.getElementsByClassName("column2");
+    if (y.style.display === "none") {
+      y.style.display = "block";
+    } else {
+      y.style.display = "none";
+    }
+  }
+
+function toggler3() {
+    var z = document.getElementsByClassName("column3");
+    if (z.style.display === "none") {
+      z.style.display = "block";
+    } else {
+      z.style.display = "none";
+    }
+  }
